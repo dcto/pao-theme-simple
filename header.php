cbaef3fc81ee8cf62a00fe8b1b0e208c00b06c4c
wasing <wasing.tao@gmail.com>
2015-01-08 01:34:40 +0800

    add css head domtree

--- header.php
+++ header.php
@@ -10,10 +10,9 @@
     'search'    =>  _t('包含关键字 %s 的文章'),
     'tag'       =>  _t('标签 %s 下的文章'),
     'author'    =>  _t('%s 发布的文章')
-), '', ' - '); ?><?php $this->options->title(); ?></title>
+), '', ' - '); ?><?php //$this->options->title(); ?></title>
 <link rel="stylesheet" href="//cdn.staticfile.org/normalize/2.1.3/normalize.min.css">
-<link rel="stylesheet" href="<?php $this->options->themeUrl('grid.css'); ?>">
-<link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('css/style.css'); ?>">
 <!--[if lt IE 9]>
 <script src="//cdn.staticfile.org/html5shiv/r29/html5.min.js"></script>
 <script src="//cdn.staticfile.org/respond.js/1.3.0/respond.min.js"></script>
@@ -21,6 +20,38 @@
 <?php $this->header(); ?>
 </head>
 <body>
+<!--[if lt IE 8]>
+    <div class="browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
+<![endif]-->
 
-</body>
-</html>
+<header id="header" class="clearfix">
+    <div class="container">
+        <div class="row">
+            <div class="site-name col-mb-12 col-9">
+                <a id="logo" href="<?php $this->options->siteUrl(); ?>">
+                    <?php if ($this->options->logoUrl): ?>
+                    <img src="<?php $this->options->logoUrl() ?>" alt="<?php $this->options->title() ?>" />
+                    <?php endif; ?>
+                    <?php $this->options->title() ?>
+                </a>
+        	    <p class="description"><?php $this->options->description() ?></p>
+            </div>
+            <div class="site-search col-3 kit-hidden-tb">
+                <form id="search" method="post" action="./" role="search">
+                    <label for="s" class="sr-only"><?php _e('搜索关键字'); ?></label>
+                    <input type="text" name="s" class="text" placeholder="<?php _e('输入关键字搜索'); ?>" />
+                    <button type="submit" class="submit"><?php _e('搜索'); ?></button>
+                </form>
+            </div>
+            <div class="col-mb-12">
+                <nav id="nav-menu" class="clearfix" role="navigation">
+                    <a<?php if($this->is('index')): ?> class="current"<?php endif; ?> href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a>
+                    <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+                    <?php while($pages->next()): ?>
+                    <a<?php if($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?> href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
+                    <?php endwhile; ?>
+                </nav>
+            </div>
+        </div><!-- end .row -->
+    </div>
+</header><!-- end #header -->
