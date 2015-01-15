b7afbbdc46eb064490310e324493fc83a8988492
wasing <wasing.tao@gmail.com>
2015-01-15 22:17:49 +0800

    add function.css and theme.css

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
 <meta name="renderer" content="webkit">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="shortcut icon" href="/favicon.ico"/>
+<link rel="apple-touch-icon" href="/touchicon.png"/>
 <title><?php $this->archiveTitle(array(
     'category'  =>  _t('分类 %s 下的文章'),
     'search'    =>  _t('包含关键字 %s 的文章'),
@@ -12,6 +14,8 @@
     'author'    =>  _t('%s 发布的文章')
 ), '', ' - '); ?><?php //$this->options->title(); ?></title>
 <link rel="stylesheet" href="<?php $this->options->themeUrl('css/reset.css'); ?>">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('css/function.css'); ?>">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('css/theme.css'); ?>">
 <?php $this->header(); ?>
 </head>
 <body>
